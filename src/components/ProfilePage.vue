5e19cd4b4ff8dd58801d04c7d0ea7dbd55da2c3e
marcuslindqvist <marcus.lindqvist@iths.se>
2021-02-12 13:31:55 +0100

    skapade upp template grund för ProfilPage

---
+++ src/components/ProfilePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <section class="header"></section>
+    <section class="profile">
+      <div class="profile-picture">
+        <img src="" alt="" />
+      </div>
+      <div class="profile-info">
+        <h3>Sixten Kaffelövér</h3>
+        <p>emailadress</p>
+      </div>
+    </section>
+    <section class="order-history">
+      <div class="order-item"></div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css" scoped>
+</style>
