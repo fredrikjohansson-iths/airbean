98121a020d679488baa0b4d46f8bd4239249f5c9
marcuslindqvist <marcus.lindqvist@iths.se>
2021-02-15 10:10:54 +0100

    lagt till menyitems

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,38 @@ export default createStore({
       name: "Marcus Lindqvist",
       email: "marcus@lindqvist.com"
     },
+    menuItems: [
+      {
+        name: "Bryggkaffe",
+        info: "Bryggd på månadens bönor",
+        price: 49
+      },
+      {
+        name: "Caffé Doppio",
+        info: "Bryggd på månadens bönor",
+        price: 49
+      },
+      {
+        name: "Cappuccino",
+        info: "Bryggd på månadens bönor",
+        price: 49
+      },
+      {
+        name: "Latte Macchiato",
+        info: "Bryggd på månadens bönor",
+        price: 49
+      },
+      {
+        name: "Kaffe Latte",
+        info: "Bryggd på månadens bönor",
+        price: 49
+      },
+      {
+        name: "Cortado",
+        info: "Bryggd på månadens bönor",
+        price: 39
+      },
+    ],
     cartItems: [],
     orderHistory: [],
     
